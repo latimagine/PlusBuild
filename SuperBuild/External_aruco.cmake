a57b50ad6d617ca3de0a80df96d405c4b63e1a16
C. G <kenavolic@protonmail.com>
2022-01-21 17:20:53 +0100

    Update to new aruco version

--- SuperBuild/External_aruco.cmake
+++ SuperBuild/External_aruco.cmake
@@ -9,11 +9,11 @@ IF(aruco_DIR)
   SET (PLUS_aruco_DIR ${aruco_DIR} CACHE INTERNAL "Path to store aruco binaries")
 ELSE()
   # aruco has not been built yet, so download and build it as an external project
-  SetGitRepositoryTag(
-    aruco
-    "https://github.com/PlusToolkit/aruco.git"
-    "master"
-    )
+  # SetGitRepositoryTag(
+  #   aruco
+  #   "${GIT_PROTOCOL}://github.com/PlusToolkit/aruco.git"
+  #   "master"
+  #   )
 
   SET (PLUS_aruco_src_DIR ${CMAKE_BINARY_DIR}/aruco CACHE INTERNAL "Path to store aruco contents")
   SET (PLUS_aruco_prefix_DIR ${CMAKE_BINARY_DIR}/aruco-prefix CACHE INTERNAL "Path to store aruco prefix data.")
@@ -25,8 +25,10 @@ ELSE()
     SOURCE_DIR "${PLUS_aruco_src_DIR}"
     BINARY_DIR "${PLUS_aruco_DIR}"
     #--Download step--------------
-    GIT_REPOSITORY ${aruco_GIT_REPOSITORY}
-    GIT_TAG ${aruco_GIT_TAG}
+    #GIT_REPOSITORY ${aruco_GIT_REPOSITORY}
+    #GIT_TAG ${aruco_GIT_TAG}
+    URL https://downloads.sourceforge.net/project/aruco/3.1.12/aruco-3.1.12.zip
+    URL_HASH SHA256=70b9ec8aa8eac6fe3f622201747a3e32c77bbb5f015e28a95c1c7c91f8ee8a09
     #--Configure step-------------
     CMAKE_ARGS
       ${ep_common_args}
@@ -38,14 +40,14 @@ ELSE()
       -DCMAKE_CXX_FLAGS:STRING=${ep_common_cxx_flags}
       -DCMAKE_C_FLAGS:STRING=${ep_common_c_flags}
       -DOpenCV_DIR:PATH=${PLUS_OpenCV_DIR}
-      -DUSE_OWN_EIGEN3=OFF
+      -DUSE_OWN_EIGEN3=ON
       -DBUILD_TESTS:BOOL=OFF
       -DBUILD_PERF_TESTS:BOOL=OFF
       -DBUILD_SHARED_LIBS:BOOL=${PLUSBUILD_BUILD_SHARED_LIBS}
+      -DCMAKE_INSTALL_PREFIX=${PLUS_aruco_DIR}
     #--Build step-----------------
     BUILD_ALWAYS 1
     #--Install step-----------------
-    INSTALL_COMMAND "" # don't install
     #--Dependencies-----------------
     DEPENDS OpenCV
   )
